f4078c639663ef8f84e4f9ca5b31918ac2ac3319
Luis Guerrero Guirado <guerrerotook@outlook.com>
2020-06-01 12:53:07 +0200

    Added the ignore route for Apple devide to remove exceptions and reduce noise.

--- Source/Web Sites/Link.BA.Donate.WebSite/Global.asax.cs
+++ Source/Web Sites/Link.BA.Donate.WebSite/Global.asax.cs
@@ -20,6 +20,7 @@ namespace Link.BA.Donate.WebSite
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*apple}", new { apple = @"(.*/)?apple-touch-icon.*\.png(/.*)?" });
 
             routes.MapRoute(
                 "Default", // Route name
