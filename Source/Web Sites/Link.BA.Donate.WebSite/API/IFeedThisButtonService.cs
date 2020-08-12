232e382017c6a395f50fc4604066b5a5f91babc3
Fábio Ferreira <fabio.ferreira@linkconsulting.com>
2020-08-12 11:02:21 +0100

    Namespace e endpoint

--- Source/Web Sites/Link.BA.Donate.WebSite/API/IFeedThisButtonService.cs
+++ Source/Web Sites/Link.BA.Donate.WebSite/API/IFeedThisButtonService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace Link.BA.Donate.WebSite.API
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFeedThisButtonService" in both code and config file together.
-    [ServiceContract]
+    [ServiceContract(Namespace = "http://alimentestaideia.pt/", Name = "FeedThisButtonService")]
     public interface IFeedThisButtonService
     {
         [OperationContract]
