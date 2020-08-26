47b2a0648ff5ff9616982e4564b6483230765da1
Luis Guerrero Guirado <luis.guerrero@microsoft.com>
2020-08-26 12:54:45 +0200

    Added cache support for the BaseData loaded in all models. Data will be cached 5 minutes

--- Source/Web Sites/Link.BA.Donate.WebSite/Controllers/BaseController.cs
+++ Source/Web Sites/Link.BA.Donate.WebSite/Controllers/BaseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using Link.BA.Donate.Models;
 
@@ -19,26 +20,51 @@ namespace Link.BA.Donate.WebSite.Controllers
             }
             else if (referenceView.Contains("Obrigado"))
             {
-                IList<SumTotalPayedDonationEntity> payedDonationEntities = donation.GetSumTotalPayedDonation();
+                IList<SumTotalPayedDonationEntity> payedDonationEntities = GetCachedElement("GetSumTotalPayedDonation", () => { return donation.GetSumTotalPayedDonation(); });
 
                 ViewBag.SumTotalDonations = payedDonationEntities[0].SumTotalPayedDonation;
             }
 
-            IList<LastPayedDonationEntity> lastPayedDonations = donation.GetLastPayedDonations(totalPayedDonations, null, null);
+            IList<LastPayedDonationEntity> lastPayedDonations = GetCachedElement($"GetLastPayedDonations{totalPayedDonations}", () => { return donation.GetLastPayedDonations(totalPayedDonations, null, null); });
 
             ViewBag.LastPayedDonations = lastPayedDonations;
 
-            IList<TotalDonationsEntity> totalDonations = donation.GetTotalDonations();
+            IList<TotalDonationsEntity> totalDonations = GetCachedElement("GetTotalDonations", () => { return donation.GetTotalDonations(); });
 
             ViewBag.TotalDonations = totalDonations;
 
-            IList<FoodBankEntity> foodBanks = donation.GetFoodBanks();
+            IList<FoodBankEntity> foodBanks = GetCachedElement("GetFoodBanks", () => { return donation.GetFoodBanks(); });
 
-            foodBanks.Insert(0, null);
+            if (foodBanks.ElementAt(0) != null)
+            {
+                foodBanks.Insert(0, null);
+            }
 
             ViewBag.FoodBanks = foodBanks;
 
-            ViewBag.ProductCatalogue = donation.GetProductCatalogue();
+            ViewBag.ProductCatalogue = GetCachedElement("GetProductCatalogue", () => { return donation.GetProductCatalogue(); });
+        }
+
+        private T GetCachedElement<T>(string key, Func<T> builder) where T : class
+        {
+            T result = default(T);
+
+            Cache cache = HttpContext.Cache;
+            object target = cache.Get(key);
+            if (target != null)
+            {
+                if (typeof(T).IsAssignableFrom(target.GetType()))
+                {
+                    result = (T)target;
+                }
+            }
+            else
+            {
+                result = builder();
+                cache.Insert(key, result, null, Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(5));
+            }
+
+            return result;
         }
     }
 }
