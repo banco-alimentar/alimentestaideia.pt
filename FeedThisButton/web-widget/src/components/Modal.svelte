f4d477e6622e84284c7cd8c1fd98b03e83624c40
Pedro Gloria <pedro.d.gloria@avanade.com>
2020-12-03 09:43:46 +0000

    removed modal privacy policy link

--- FeedThisButton/web-widget/src/components/Modal.svelte
+++ FeedThisButton/web-widget/src/components/Modal.svelte
@@ -127,9 +127,9 @@
                     <div class="logo">
                         <LogoBa />
                     </div>
-                    <div class="links text xsmall alt">
+                    <!-- <div class="links text xsmall alt">
                         <a href="https://www.bancoalimentar.pt/politica-de-privacidade-e-protecao-de-dados/" target="_blank" rel="noopener noreferrer">Política de privacidade</a>
-                    </div>
+                    </div> -->
                     <div class="btn">
                         <RoundButton responsive={false} alt large text={nextButtonText} image="seta" imageRight 
                             on:click={onContinue} disabled={continueButtonDisabled}
