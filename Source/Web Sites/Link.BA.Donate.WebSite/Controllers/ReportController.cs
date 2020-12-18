3c7c8cb878e095b2b6bc6438c48594f718fbadba
Tiago Andrade e Silva <tiagonmas>
2020-12-18 10:26:16 +0000

    Added auth settings to web.release.config. Removed some info from viewbag that was not used.

--- Source/Web Sites/Link.BA.Donate.WebSite/Controllers/ReportController.cs
+++ Source/Web Sites/Link.BA.Donate.WebSite/Controllers/ReportController.cs
@@ -41,15 +41,6 @@ namespace Link.BA.Donate.WebSite.Controllers
             //You get the user’s first and last name below:
             ViewBag.Name = userClaims?.FindFirst("name")?.Value;
 
-            // The 'preferred_username' claim can be used for showing the username
-            ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
-
-            // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
-            ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
-
-            // TenantId is the unique Tenant Id - which represents an organization in Azure AD
-            ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
-
             return View();
         }
 
