59b6fae0981223274cb0cb7d35556e80239e3600
Tiago Andrade e Silva <tiagonmas>
2021-04-05 22:46:07 +0100

    Added InvoiceModelTests and removed some comments from ConvertAmountToText

---
+++ dotnetcore/BancoAlimentar.AlimentaEstaIdeia.WebTests/InvoiceModelTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BancoAlimentar.AlimentaEstaIdeia.Web.Areas.Identity.Pages.Account.Manage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using BancoAlimentar.AlimentaEstaIdeia.Repository;
+using Microsoft.EntityFrameworkCore;
+using BancoAlimentar.AlimentaEstaIdeia.Model;
+using Microsoft.AspNetCore.Identity;
+using BancoAlimentar.AlimentaEstaIdeia.Model.Identity;
+using Microsoft.Extensions.Localization;
+
+namespace BancoAlimentar.AlimentaEstaIdeia.Web.Areas.Identity.Pages.Account.Manage.Tests
+{
+    [TestClass()]
+    public class InvoiceModelTests
+    {
+
+        IConfiguration Configuration { get; set; }
+        ServiceCollection ServiceCollection { get; set; }
+
+        ServiceProvider ServiceProvider { get; set; }
+
+        public InvoiceModelTests()
+        {
+            ServiceCollection = new ServiceCollection();
+
+            // the type specified here is just so the secrets library can 
+            // find the UserSecretId we added in the csproj file
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<InvoiceModelTests>();
+
+            Configuration = builder.Build();
+
+            ServiceCollection.AddScoped<DonationRepository>();
+            ServiceCollection.AddScoped<ProductCatalogueRepository>();
+            ServiceCollection.AddScoped<FoodBankRepository>();
+            ServiceCollection.AddScoped<DonationItemRepository>();
+            ServiceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            ServiceCollection.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("BancoAlimentar.AlimentaEstaIdeia.Web")));
+
+            ServiceProvider = ServiceCollection.BuildServiceProvider();
+        }
+
+        [TestMethod()]
+        public void ConvertAmountToTextTest()
+        {
+            IUnitOfWork context = this.ServiceProvider.GetRequiredService<IUnitOfWork>();
+            UserManager<WebUser> userManager = this.ServiceProvider.GetRequiredService<UserManager<WebUser>>();
+            IStringLocalizerFactory strLocalizer = this.ServiceProvider.GetRequiredService<IStringLocalizerFactory>();
+
+            InvoiceModel x = new InvoiceModel(userManager, context, strLocalizer);
+
+            Invoice invoice = new Invoice();
+            x.Invoice = invoice;
+            invoice.Donation = new Donation();
+
+            invoice.Donation.DonationAmount = 12;            
+            x.ConvertAmountToText();
+            Assert.AreEqual(x.DonationAmountToText, "Doze Euros");
+
+            invoice.Donation.DonationAmount = 12.5;
+            x.ConvertAmountToText();
+            Assert.AreEqual(x.DonationAmountToText, "Doze Euros e Cinquenta cêntimos");
+        }
+    }
+}
+
