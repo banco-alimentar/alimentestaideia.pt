e4f61714a51156acf453dee609b7d6955ccfce01
Luis Guerrero Guirado <guerrerotook@outlook.com>
2021-04-05 11:03:32 +0200

    Added the client side validation in javascript for the minimum of 50 cents of donation. #188

---
+++ dotnetcore/BancoAlimentar.AlimentaEstaIdeia.Web/wwwroot/js/MinimumValueValidate.js
@@ -0,0 +1,13 @@
+﻿jQuery.validator.addMethod("minvalue",
+    function (value, element, param) {
+        var amount = parseFloat(value);
+        var minimumValue = parseFloat(element.attributes["data-val-minvalue-minvalue"].nodeValue);
+        if (amount < minimumValue) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    });
+
+jQuery.validator.unobtrusive.adapters.addBool("minvalue");
