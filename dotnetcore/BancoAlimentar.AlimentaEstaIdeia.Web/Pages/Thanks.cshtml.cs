5fbd264c83f538658ac045aad84480f22b6bf79c
Tiago Andrade e Silva <tiagonmas>
2021-04-07 21:03:26 +0100

    More Language settings

--- dotnetcore/BancoAlimentar.AlimentaEstaIdeia.Web/Pages/Thanks.cshtml.cs
+++ dotnetcore/BancoAlimentar.AlimentaEstaIdeia.Web/Pages/Thanks.cshtml.cs
@@ -117,7 +117,7 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Web.Pages
                     this.configuration["Email.PaymentToDonor.Subject"],
                     email,
                     pdfFile.Item2,
-                    $"RECIBO Nº B{DateTime.Now.Year}-{pdfFile.Item1.Id}.pdf",
+                    $"RECIBO Nº AA{pdfFile.Item1.Id}-{DateTime.Now.Year}.pdf",
                     this.configuration);
             }
             else
