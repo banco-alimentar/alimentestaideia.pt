073fa55c815212119251b3f62fed33b4ddfc4a3f
Luis Guerrero Guirado <guerrerotook@outlook.com>
2021-05-18 16:56:08 +0200

    Added subscription tests

--- BancoAlimentar.AlimentaEstaIdeia.RepositoryTests/SubscriptionTests.cs
+++ BancoAlimentar.AlimentaEstaIdeia.RepositoryTests/SubscriptionTests.cs
@@ -91,6 +91,96 @@
                 DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture));
 
             var response = subscriptionPaymentApi.SubscriptionPost(subscription);
+            Assert.AreEqual(response.Status, "ok");
+            Assert.AreEqual(response.Method.Status, PaymentRecurringResponseMethod.StatusEnum.Waiting);
+            CollectionAssert.AllItemsAreNotNull(response.Messages);
+            Assert.IsNotNull(response.Id);
+
+            subscriptionPaymentApi.SubscriptionIdDelete(response.Id.ToString());
+        }
+
+        [TestMethod]
+        public void GetSubscriptionFromIdTest()
+        {
+            PaymentSubscription subscription = new PaymentSubscription(
+                Guid.NewGuid(),
+                new SubscriptionCapture("my subscription1", Guid.NewGuid().ToString(), new SinglePaymentRequestCapture("Alimente esta ideia Donation", Guid.NewGuid().ToString())),
+                DateTime.UtcNow.AddYears(1).ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                PaymentSubscription.CurrencyEnum.EUR,
+                new Customer(default(Guid),
+                    "customer1",
+                    "email@customer.com",
+                    "123456789",
+                    "+351",
+                    "123456789",
+                    Guid.NewGuid().ToString(),
+                    "PT"),
+                Guid.NewGuid().ToString(),
+                18.3d,
+                PaymentSubscription.FrequencyEnum._1M,
+                0,
+                DateTime.UtcNow.AddDays(1).ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                false,
+                true,
+                0,
+                PaymentSubscriptionMethodAvailable.Cc,
+                null,
+                DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture));
+
+            var response = subscriptionPaymentApi.SubscriptionPost(subscription);
+            Assert.AreEqual(response.Status, "ok");
+            Assert.AreEqual(response.Method.Status, PaymentRecurringResponseMethod.StatusEnum.Waiting);
+            CollectionAssert.AllItemsAreNotNull(response.Messages);
+            Assert.IsNotNull(response.Id);
+
+            var subscriptionResponse = subscriptionPaymentApi.SubscriptionIdGet(response.Id.ToString());
+
+            subscriptionPaymentApi.SubscriptionIdDelete(response.Id.ToString());
+        }
+
+        [TestMethod]
+        public void PatchSubscriptionFromIdTest()
+        {
+            PaymentSubscription subscription = new PaymentSubscription(
+                Guid.NewGuid(),
+                new SubscriptionCapture("my subscription1", Guid.NewGuid().ToString(), new SinglePaymentRequestCapture("Alimente esta ideia Donation", Guid.NewGuid().ToString())),
+                DateTime.UtcNow.AddYears(1).ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                PaymentSubscription.CurrencyEnum.EUR,
+                new Customer(default(Guid),
+                    "customer1",
+                    "email@customer.com",
+                    "123456789",
+                    "+351",
+                    "123456789",
+                    Guid.NewGuid().ToString(),
+                    "PT"),
+                Guid.NewGuid().ToString(),
+                18.3d,
+                PaymentSubscription.FrequencyEnum._1M,
+                0,
+                DateTime.UtcNow.AddDays(1).ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                false,
+                true,
+                0,
+                PaymentSubscriptionMethodAvailable.Cc,
+                null,
+                DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture));
+
+            var response = subscriptionPaymentApi.SubscriptionPost(subscription);
+            Assert.AreEqual(response.Status, "ok");
+            Assert.AreEqual(response.Method.Status, PaymentRecurringResponseMethod.StatusEnum.Waiting);
+            CollectionAssert.AllItemsAreNotNull(response.Messages);
+            Assert.IsNotNull(response.Id);
+
+            double newAmount = 25.36d;
+
+            PaymentSubscriptionPatchable patch = new PaymentSubscriptionPatchable();
+            patch.Value = newAmount;
+
+            subscriptionPaymentApi.SubscriptionIdPatch(response.Id.ToString(), patch);
+            var subscriptionResponse = subscriptionPaymentApi.SubscriptionIdGet(response.Id.ToString());
+            Assert.AreEqual(subscriptionResponse.Value, newAmount);
+            subscriptionPaymentApi.SubscriptionIdDelete(response.Id.ToString());
         }
     }
 }
