f05b167572ed741d532f570b869c8653242b1884
Luis Guerrero Guirado <guerrerotook@outlook.com>
2021-08-16 17:46:56 +0200

    Added the tool projects.

---
+++ BancoAlimentar.AlimentaEstaIdeia.Tools/BaseDatabaseTool.cs
@@ -0,0 +1,25 @@
+﻿namespace BancoAlimentar.AlimentaEstaIdeia.Tools
+{
+    using BancoAlimentar.AlimentaEstaIdeia.Model;
+    using BancoAlimentar.AlimentaEstaIdeia.Repository;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class BaseDatabaseTool
+    {
+        public BaseDatabaseTool(ApplicationDbContext context, IUnitOfWork unitOfWork)
+        {
+            Context = context;
+            UnitOfWork = unitOfWork;
+        }
+
+        protected ApplicationDbContext Context { get; }
+
+        protected IUnitOfWork UnitOfWork { get; }
+
+        public abstract void ExecuteTool();
+    }
+}
---
+++ BancoAlimentar.AlimentaEstaIdeia.Tools/Database/ConsolidateConfirmedPaymentIdProd.cs
@@ -0,0 +1,41 @@
+﻿namespace BancoAlimentar.AlimentaEstaIdeia.Tools.Database
+{
+    using BancoAlimentar.AlimentaEstaIdeia.Model;
+    using BancoAlimentar.AlimentaEstaIdeia.Repository;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ConsolidateConfirmedPaymentIdProd : BaseDatabaseTool
+    {
+        public ConsolidateConfirmedPaymentIdProd(ApplicationDbContext context, IUnitOfWork unitOfWork) : base(context, unitOfWork)
+        {
+        }
+
+        public override void ExecuteTool()
+        {
+            var donations = this.Context.Donations
+                .Where(p => p.ConfirmedPayment == null)
+                .ToList();
+
+            foreach (var donation in donations)
+            {
+                var payments = this.Context.PaymentItems
+                    .Where(p => p.Donation == donation)
+                    .Select(p => p.Payment)
+                    .ToList();
+
+                BasePayment currentPayment = payments
+                    .Where(p => PaymentStatusMessages.SuccessPaymentMessages.Any(i => i == p.Status))
+                    .FirstOrDefault();
+
+                if (currentPayment != null)
+                {
+                    donation.ConfirmedPayment = currentPayment;
+                }
+            }
+        }
+    }
+}
