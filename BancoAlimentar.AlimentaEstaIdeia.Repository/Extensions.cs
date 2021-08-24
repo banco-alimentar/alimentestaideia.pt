f5eb02c674bd0df11b704d97800ea32325276d45
Luis Guerrero Guirado <guerrerotook@outlook.com>
2021-08-24 17:48:58 +0200

    Subscription display relevant information. Better handle of the initial donation payment for subscriptions.

---
+++ BancoAlimentar.AlimentaEstaIdeia.Repository/Extensions.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Extensions.cs" company="Federação Portuguesa dos Bancos Alimentares Contra a Fome">
+// Copyright (c) Federação Portuguesa dos Bancos Alimentares Contra a Fome. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace BancoAlimentar.AlimentaEstaIdeia.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Class for extensions methods.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Gets the Portugal date time, based on the <see cref="DateTime"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="DateTime"/> to convert the date to Portugal datetime.</param>
+        /// <returns>A instance of <see cref="DateTime"/> in Portugal datetime.</returns>
+        public static DateTime GetPortugalDateTime(this DateTime value)
+        {
+            return TimeZoneInfo.ConvertTime(value, TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"));
+        }
+    }
+}
