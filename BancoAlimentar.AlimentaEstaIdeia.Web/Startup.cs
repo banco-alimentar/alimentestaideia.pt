4fe3e2c72819ce7cc01264c164a3efaaf7be773b
kshitijcode <ikshitijsharma@gmail.com>
2021-09-01 16:57:20 +0530

    Adding Anti Forgery in Services

--- BancoAlimentar.AlimentaEstaIdeia.Web/Startup.cs
+++ BancoAlimentar.AlimentaEstaIdeia.Web/Startup.cs
@@ -57,6 +57,7 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAntiforgery();
             services.AddScoped<DonationRepository>();
             services.AddFeatureManagement();
             services.AddScoped<ProductCatalogueRepository>();
@@ -287,12 +288,6 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Use(async (context, next) =>
-            {
-                context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
-                await next();
-            });
-            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
