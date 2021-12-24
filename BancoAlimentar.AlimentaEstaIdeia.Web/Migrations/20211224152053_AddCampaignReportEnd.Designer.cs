35c1e562d97c7adc576490f8d576484ba2aef5b3
Tiago Andrade e Silva <tiagonmas@hotmail.com>
2021-12-24 15:41:48 +0000

    #580 new Campaign.ReportEnd field and Admin updates to manage the field

--- BancoAlimentar.AlimentaEstaIdeia.Web/Migrations/20211224152053_AddCampaignReportEnd.Designer.cs
+++ BancoAlimentar.AlimentaEstaIdeia.Web/Migrations/20211224152053_AddCampaignReportEnd.Designer.cs
@@ -4,6 +4,7 @@ using BancoAlimentar.AlimentaEstaIdeia.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,15 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BancoAlimentar.AlimentaEstaIdeia.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211224152053_AddCampaignReportEnd")]
+    partial class AddCampaignReportEnd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+
+        /// <summary>
+        /// EF auto.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BancoAlimentar.AlimentaEstaIdeia.Web/Migrations/20211224152053_AddCampaignReportEnd.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="20211224152053_AddCampaignReportEnd.cs" company="Federação Portuguesa dos Bancos Alimentares Contra a Fome">
+// Copyright (c) Federação Portuguesa dos Bancos Alimentares Contra a Fome. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace BancoAlimentar.AlimentaEstaIdeia.Web.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+#nullable disable
+
+    /// <summary>
+    /// add a campaign report end date.
+    /// </summary>
+    public partial class AddCampaignReportEnd : Migration
+    {
+        /// <summary>
+        /// Move Up.
+        /// </summary>
+        /// <param name="migrationBuilder">migrationBuilder.</param>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReportEnd",
+                table: "Campaigns",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <summary>
+        /// Move Down migration.
+        /// </summary>
+        /// <param name="migrationBuilder">migrationBuilder.</param>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReportEnd",
+                table: "Campaigns");
+        }
+    }
+}
