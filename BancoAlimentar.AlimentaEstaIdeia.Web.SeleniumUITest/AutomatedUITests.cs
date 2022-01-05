1eacdfee3ed76cd8c5b9910af9f220a1d0cce7f4
Tiago Andrade e Silva <tiagonmas@hotmail.com>
2022-01-05 19:17:56 +0000

    improvement to UI tests

--- BancoAlimentar.AlimentaEstaIdeia.Web.SeleniumUITest/AutomatedUITests.cs
+++ BancoAlimentar.AlimentaEstaIdeia.Web.SeleniumUITest/AutomatedUITests.cs
@@ -77,115 +77,43 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Web.SeleniumUITest
             this.driver.Dispose();
         }
 
-        private void CreateDonation(double testAmmount, String testUserEmail, String testUserName, String testCompany)
+        private void CreateDonation(DonationTestData dData)
         {
 
-
-            Actions builder = new Actions(driver);
-
+            // Set Donation amount
             driver.Navigate().GoToUrl(baseUrl+"/Donation");
 
+            //driver.FindElement(By.Id("total")).Clear();
+            //{
+            //    var element = driver.FindElement(By.Id("total"));
+            //    js.ExecuteScript("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '" + dData.testAmmount + "'}", element);
+            //}
 
+            //driver.FindElement(By.Id("total")).SendKeys(" ");//Force calculateChange event
+            driver.FindElement(By.CssSelector(".boxed:nth-child(1) .more")).Click();
+            driver.FindElement(By.CssSelector(".boxed:nth-child(4) .more")).Click();
+            driver.FindElement(By.CssSelector(".boxed:nth-child(5) .more")).Click();
+            driver.FindElement(By.CssSelector(".boxed:nth-child(2) .more")).Click();
+            driver.FindElement(By.CssSelector(".boxed:nth-child(3) .more")).Click();
             driver.FindElement(By.CssSelector(".boxed:nth-child(6) .more")).Click();
+
             driver.FindElement(By.Id("FoodBankId")).Click();
             {
                 var dropdown = driver.FindElement(By.Id("FoodBankId"));
                 dropdown.FindElement(By.XPath("//option[. = 'Lisboa']")).Click();
             }
             driver.FindElement(By.Id("Name")).Click();
-            driver.FindElement(By.Id("Name")).SendKeys(testUserName);
-            driver.FindElement(By.Id("CompanyName")).SendKeys(testCompany);
-            driver.FindElement(By.Id("Email")).SendKeys(testUserEmail);
+            driver.FindElement(By.Id("Name")).SendKeys(dData.testUserName);
+            driver.FindElement(By.Id("CompanyName")).SendKeys(dData.testCompany);
+            driver.FindElement(By.Id("Email")).SendKeys(dData.testUserEmail);
 
             js.ExecuteScript("document.querySelector('#AcceptsTermsCheckBox').checked = true");
 
             driver.FindElement(By.CssSelector(".text3 > span")).Click();
         }
 
-        [Fact]
-        public void Visa()
+        private void ConfirmDonation(DonationTestData dData)
         {
-            Double testAmmount = 0.6;
-            String testUserEmail = "alimentestaideia.dev@outlook.com";
-            String testCompany = "Test Company";
-            String testUserName = "Antonio Manuel Teste";
-            CreateDonation(testAmmount, testUserEmail, testUserName, testCompany);
-
-            driver.FindElement(By.CssSelector("#pagamentounicre > .pmethod-img")).Click();
-            driver.FindElement(By.CssSelector("form:nth-child(3) > .payment-action:nth-child(2) > span")).Click();
-            driver.FindElement(By.CssSelector(".col-xs-12 > .btn")).Click();
-            driver.FindElement(By.Name("cardholder")).Click();
-            driver.FindElement(By.Name("cardholder")).SendKeys(testUserName);
-            {
-                var dropdown = driver.FindElement(By.Name("card_expiration_month"));
-                dropdown.FindElement(By.XPath("//option[. = '07']")).Click();
-            }
-            {
-                var dropdown = driver.FindElement(By.Name("card_expiration_year"));
-                dropdown.FindElement(By.XPath("//option[. = '2026']")).Click();
-            }
-            driver.FindElement(By.Name("card_cvv")).Click();
-            driver.FindElement(By.Name("card_cvv")).SendKeys("003");
-            driver.FindElement(By.Name("card_number")).Click();
-            {
-                var dropdown = driver.FindElement(By.Name("card_number"));
-                dropdown.FindElement(By.XPath("//option[. = '0000000000000000 - success']")).Click();
-            }
-            driver.FindElement(By.Name("accept_privacy_policy")).Click();
-            driver.FindElement(By.Id("phone")).Click();
-            driver.FindElement(By.Id("phone")).SendKeys("1231231232");
-            driver.FindElement(By.CssSelector(".action-buttons .btn-primary")).Click();
-
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-            wait.Until(ExpectedConditions.UrlMatches("Thanks"));
-
-
-            Uri theUri = new Uri(this.driver.Url);
-            String pid = System.Web.HttpUtility.ParseQueryString(theUri.Query).Get("PublicId");
-
-            Assert.NotNull(pid);
-
-            var donations = this.myApplicationDbContext.Donations
-                .Include(p => p.ConfirmedPayment)
-                .Include(p => p.User)
-                .Where(p => p.PublicId == new Guid(pid))
-                .ToList();
-
-            Assert.Single(donations);
-
-            var donation = donations.FirstOrDefault<Donation>();
-            Assert.NotNull(donation);
-            Assert.NotNull(donation.ConfirmedPayment.Completed);
-            Assert.Equal(PaymentStatus.Payed, donation.PaymentStatus);
-            Assert.Equal(testAmmount, donation.DonationAmount);
-            Assert.Equal(testUserEmail, donation.User.Email);
-            Assert.Equal(testCompany, donation.User.CompanyName);
-            Assert.Equal(testUserName, donation.User.FullName);
-        }
-
-        [Fact]
-        public void DonationMbWay()
-        {
-            Double testAmmount = 0.6;
-            String testUserEmail= "alimentestaideia.dev@outlook.com";
-            String testCompany = "Test Company";
-            String testUserName = "Antonio Manuel Teste";
-            CreateDonation(testAmmount, testUserEmail, testUserName, testCompany);
-
-            driver.FindElement(By.CssSelector("#pagamentombway > .pmethod-img")).Click();
-            driver.FindElement(By.Id("PhoneNumber")).Click();
-
-            
-            js.ExecuteScript("document.querySelector('#PhoneNumber').value = ''");
-            driver.FindElement(By.Id("PhoneNumber")).SendKeys("911234567");
-
-            driver.FindElement(By.CssSelector(".payment-form > .payment-action > span")).Click();
-
-
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-            wait.Until(ExpectedConditions.UrlMatches("Payments"));
-            wait.Until(ExpectedConditions.UrlMatches("Thanks"));
-
             Assert.Contains("Thanks", this.driver.Url);
 
             Uri theUri = new Uri(this.driver.Url);
@@ -205,10 +133,140 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Web.SeleniumUITest
             Assert.NotNull(donation);
             Assert.NotNull(donation.ConfirmedPayment.Completed);
             Assert.Equal(PaymentStatus.Payed, donation.PaymentStatus);
-            Assert.Equal(testAmmount, donation.DonationAmount );
-            Assert.Equal(testUserEmail,donation.User.Email);
-            Assert.Equal(testCompany, donation.User.CompanyName);
-            Assert.Equal(testUserName, donation.User.FullName);
+            Assert.Equal(dData.testAmmount, donation.DonationAmount);
+            Assert.Equal(dData.testUserEmail, donation.User.Email);
+            Assert.Equal(dData.testCompany, donation.User.CompanyName);
+            Assert.Equal(dData.testUserName, donation.User.FullName);
+        }
+
+
+
+        [Fact]
+        public void Visa_Anonymous_Donation()
+        {
+            // Arrange
+            var donationData = new DonationTestData(9, "alimentestaideia.dev@outlook.com", "Antonio Manuel Teste Visa", "Test Company");
+
+            // Act
+            CreateDonation(donationData);
+
+            driver.FindElement(By.CssSelector("#pagamentounicre > .pmethod-img")).Click();
+            driver.FindElement(By.CssSelector("form:nth-child(3) > .payment-action:nth-child(2) > span")).Click();
+            driver.FindElement(By.CssSelector(".col-xs-12 > .btn")).Click();
+            driver.FindElement(By.Name("cardholder")).Click();
+            driver.FindElement(By.Name("cardholder")).SendKeys(donationData.testUserName);
+            {
+                var dropdown = driver.FindElement(By.Name("card_expiration_month"));
+                dropdown.FindElement(By.XPath("//option[. = '07']")).Click();
+            }
+            {
+                var dropdown = driver.FindElement(By.Name("card_expiration_year"));
+                dropdown.FindElement(By.XPath("//option[. = '2026']")).Click();
+            }
+            driver.FindElement(By.Name("card_cvv")).Click();
+            driver.FindElement(By.Name("card_cvv")).SendKeys("003");
+            driver.FindElement(By.Name("card_number")).Click();
+            {
+                var dropdown = driver.FindElement(By.Name("card_number"));
+                dropdown.FindElement(By.XPath("//option[. = '0000000000000000 - success']")).Click();
+            }
+            driver.FindElement(By.Name("accept_privacy_policy")).Click();
+
+            js.ExecuteScript("document.querySelector('#phone').value = ''");
+            js.ExecuteScript("document.querySelector('#name').value = ''");
+            js.ExecuteScript("document.querySelector('#email').value = ''");
+
+            driver.FindElement(By.Id("phone")).Click();
+            driver.FindElement(By.Id("phone")).SendKeys("1231231232");
+
+            driver.FindElement(By.Id("name")).Click();
+            driver.FindElement(By.Id("name")).SendKeys(donationData.testUserName);
+
+            driver.FindElement(By.Id("email")).SendKeys(donationData.testUserEmail);
+
+            driver.FindElement(By.CssSelector(".action-buttons .btn-primary")).Click();
+            driver.FindElement(By.CssSelector(".col-xs-12:nth-child(1) .btn-primary")).Click();
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            wait.Until(ExpectedConditions.UrlMatches("Thanks"));
+
+            //Verify
+            ConfirmDonation(donationData);
+        }
+
+        [Fact]
+        public void Paypal_Anononymous_Donation()
+        {
+            // Arrange
+            Actions builder = new Actions(driver);
+            var donationData = new DonationTestData(9, "alimentestaideia.dev@outlook.com", "Antonio Manuel Teste Paypal", "Test Company");
+
+            // Act
+            CreateDonation(donationData);
+
+            driver.FindElement(By.CssSelector("#pagamentopaypal > .pmethod-img")).Click();
+
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).SendKeys("sb-xkcxs2177214@personal.example.com");
+
+            {
+                var element = driver.FindElement(By.Id("btnNext"));
+                builder.MoveToElement(element).Perform();
+            }
+            driver.FindElement(By.Id("btnNext")).Click();
+
+            js.ExecuteScript("document.querySelector('#password').value = 'CM891Yg#'");
+
+            driver.FindElement(By.Id("btnLogin")).Click();
+            driver.FindElement(By.Id("payment-submit-btn")).Click();
+
+            // Verify
+            ConfirmDonation(donationData);
+        }
+
+        [Fact]
+        public void MbWay_Anonymous_Donation()
+        {
+            // Arrange
+            var donationData = new DonationTestData(9, "alimentestaideia.dev@outlook.com", "Antonio Manuel Teste", "Test Company");
+
+            // Act
+            CreateDonation(donationData);
+
+            driver.FindElement(By.CssSelector("#pagamentombway > .pmethod-img")).Click();
+            driver.FindElement(By.Id("PhoneNumber")).Click();
+
+            
+            js.ExecuteScript("document.querySelector('#PhoneNumber').value = ''");
+            driver.FindElement(By.Id("PhoneNumber")).SendKeys("911234567");
+
+            driver.FindElement(By.CssSelector(".payment-form > .payment-action > span")).Click();
+
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.UrlMatches("Payments"));
+            wait.Until(ExpectedConditions.UrlMatches("Thanks"));
+
+            // Verify
+            ConfirmDonation(donationData);
+        }
+    }
+
+    public class DonationTestData{
+        public double testAmmount { get; set; }
+        public string testUserEmail { get; set; }
+
+        public string testUserName { get; set; }
+
+        public string testCompany { get; set; }
+
+
+        public DonationTestData(double _testAmmount, String _testUserEmail, String _testUserName, String _testCompany)
+        {
+            testAmmount=_testAmmount;
+            testUserEmail=_testUserEmail;
+            testUserName = _testUserName;
+            testCompany = _testCompany;
         }
     }
 }
