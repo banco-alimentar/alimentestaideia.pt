1d44a2e15b34ae088455e89dbcb3270787117dba
Luis Guerrero Guirado <guerrerotook@outlook.com>
2022-01-18 13:09:13 +0100

    Disable Application Insights when executing selenium test. Enable optional secrets.json

---
+++ BancoAlimentar.AlimentaEstaIdeia.Web/Pages/SeleniumTests.cshtml.cs
@@ -0,0 +1,25 @@
+// -----------------------------------------------------------------------
+// <copyright file="SeleniumTests.cshtml.cs" company="Federação Portuguesa dos Bancos Alimentares Contra a Fome">
+// Copyright (c) Federação Portuguesa dos Bancos Alimentares Contra a Fome. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace BancoAlimentar.AlimentaEstaIdeia.Web.Pages
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    /// <summary>
+    /// Disable AI on the selenium tests.
+    /// </summary>
+    public class SeleniumTestsModel : PageModel
+    {
+        /// <summary>
+        /// Execute the get operation.
+        /// </summary>
+        public void OnGet()
+        {
+            HttpContext.Session.SetString("DisableAI", "true");
+        }
+    }
+}
