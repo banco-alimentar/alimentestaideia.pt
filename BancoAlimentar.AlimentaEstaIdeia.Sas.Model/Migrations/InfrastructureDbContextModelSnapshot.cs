10a3a21b0e030eb554cfd5e6ccfda15b4d80c29d
Siobhan Baynes <siba@microsoft.com>
2022-05-18 16:04:14 -0700

    Remove constructors

--- BancoAlimentar.AlimentaEstaIdeia.Sas.Model/Migrations/InfrastructureDbContextModelSnapshot.cs
+++ BancoAlimentar.AlimentaEstaIdeia.Sas.Model/Migrations/InfrastructureDbContextModelSnapshot.cs
@@ -47,64 +47,7 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Sas.Model.Migrations
                     b.HasIndex("TenantId");
 
                     b.ToTable("DomainIdentifiers");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 3,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7495),
-                            DomainName = "localhost",
-                            Environment = "Development",
-                            TenantId = 5
-                        },
-                        new
-                        {
-                            Id = 4,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7495),
-                            DomainName = "doar-dev.bancoalimentar.pt",
-                            Environment = "Development",
-                            TenantId = 7
-                        },
-                        new
-                        {
-                            Id = 5,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7496),
-                            DomainName = "alimentaestaideia-beta.azurewebsites.net",
-                            Environment = "Staging",
-                            TenantId = 10
-                        },
-                        new
-                        {
-                            Id = 6,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7496),
-                            DomainName = "doar-dev.alimentestaideia.pt",
-                            Environment = "Staging",
-                            TenantId = 10
-                        },
-                        new
-                        {
-                            Id = 7,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7497),
-                            DomainName = "alimentaestaideia-developer.azurewebsites.net",
-                            Environment = "Staging",
-                            TenantId = 14
-                        },
-                        new
-                        {
-                            Id = 8,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7497),
-                            DomainName = "alimentaestaideia-developer.azurewebsites.net",
-                            Environment = "Development",
-                            TenantId = 14
-                        },
-                        new
-                        {
-                            Id = 9,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7497),
-                            DomainName = "doar-dev.bancoalimentar.pt",
-                            Environment = "Staging",
-                            TenantId = 7
-                        });
+                    
                 });
 
             modelBuilder.Entity("BancoAlimentar.AlimentaEstaIdeia.Sas.Model.InvoiceConfiguration", b =>
@@ -152,63 +95,6 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Sas.Model.Migrations
 
                     b.ToTable("KeyVaultConfigurations");
 
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7468),
-                            Environment = "Development",
-                            TenantId = 7,
-                            Vault = "doarbancoalimentar"
-                        },
-                        new
-                        {
-                            Id = 7,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7470),
-                            Environment = "Development",
-                            TenantId = 5,
-                            Vault = "doarbancoalimentar"
-                        },
-                        new
-                        {
-                            Id = 8,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7471),
-                            Environment = "Development",
-                            TenantId = 10,
-                            Vault = "doaralimentestaideia"
-                        },
-                        new
-                        {
-                            Id = 9,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7472),
-                            Environment = "Development",
-                            TenantId = 13,
-                            Vault = "doaralimentestaideia"
-                        },
-                        new
-                        {
-                            Id = 10,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7473),
-                            Environment = "Staging",
-                            TenantId = 10,
-                            Vault = "doaralimentestaideia"
-                        },
-                        new
-                        {
-                            Id = 11,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7474),
-                            Environment = "Staging",
-                            TenantId = 7,
-                            Vault = "doarbancoalimentar"
-                        },
-                        new
-                        {
-                            Id = 12,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7475),
-                            Environment = "Staging",
-                            TenantId = 14,
-                            Vault = "alimentaestaideia-key"
-                        });
                 });
 
             modelBuilder.Entity("BancoAlimentar.AlimentaEstaIdeia.Sas.Model.Tenant", b =>
@@ -244,71 +130,6 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Sas.Model.Migrations
                     b.HasIndex("InvoiceConfigurationId");
 
                     b.ToTable("Tenants");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 5,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7283),
-                            InvoicingStrategy = "MultipleTablesPerFoodBank",
-                            Name = "localhost",
-                            PaymentStrategy = "IndividualPaymentProcessorPerFoodBank",
-                            PublicId = new Guid("9d46682c-588b-45ce-8829-f8ce771dc10e")
-                        },
-                        new
-                        {
-                            Id = 7,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7288),
-                            InvoicingStrategy = "MultipleTablesPerFoodBank",
-                            Name = "bancoalimentar",
-                            PaymentStrategy = "SharedPaymentProcessor",
-                            PublicId = new Guid("2d4d6448-71d3-454a-a584-9ebfc0b7ede5")
-                        },
-                        new
-                        {
-                            Id = 8,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7334),
-                            InvoicingStrategy = "SingleInvoiceTable",
-                            Name = "alimentaestaideia-beta",
-                            PaymentStrategy = "SharedPaymentProcessor",
-                            PublicId = new Guid("bd31d165-b8df-4c7a-a5e3-5e3d155948e2")
-                        },
-                        new
-                        {
-                            Id = 9,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7335),
-                            InvoicingStrategy = "SingleInvoiceTable",
-                            Name = "alimentaestaideia-beta",
-                            PaymentStrategy = "SharedPaymentProcessor",
-                            PublicId = new Guid("de68a683-0cd2-44ce-b9c6-505aabbcdfc3")
-                        },
-                        new
-                        {
-                            Id = 10,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7336),
-                            InvoicingStrategy = "SingleInvoiceTable",
-                            Name = "doar-dev.alimentestaideia.pt",
-                            PaymentStrategy = "SharedPaymentProcessor",
-                            PublicId = new Guid("03317653-9140-4cc0-91e0-2b2aa2a8e5fe")
-                        },
-                        new
-                        {
-                            Id = 13,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7338),
-                            InvoicingStrategy = "SingleInvoiceTable",
-                            Name = "doar-dev.alimentestaideia-dev.pt",
-                            PaymentStrategy = "SharedPaymentProcessor",
-                            PublicId = new Guid("f3aea354-b2ad-4451-893f-891dfb2c6c99")
-                        },
-                        new
-                        {
-                            Id = 14,
-                            Created = new DateTime(2022, 5, 18, 17, 11, 59, 395, DateTimeKind.Utc).AddTicks(7340),
-                            InvoicingStrategy = "SingleInvoiceTable",
-                            Name = "alimentaestaideia-developer.azurewebsites.net",
-                            PaymentStrategy = "SharedPaymentProcessor",
-                            PublicId = new Guid("f904b771-b750-4392-a8f6-f76a8b9cc1be")
-                        });
                 });
 
             modelBuilder.Entity("BancoAlimentar.AlimentaEstaIdeia.Sas.Model.TenantConfiguration", b =>
