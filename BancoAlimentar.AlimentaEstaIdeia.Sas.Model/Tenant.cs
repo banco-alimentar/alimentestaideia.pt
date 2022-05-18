3eb5364395055d51b5219911eb50402cd4087ee0
Siobhan Baynes <siba@microsoft.com>
2022-05-18 16:04:57 -0700

    Remove constructors

--- BancoAlimentar.AlimentaEstaIdeia.Sas.Model/Tenant.cs
+++ BancoAlimentar.AlimentaEstaIdeia.Sas.Model/Tenant.cs
@@ -31,22 +31,6 @@ namespace BancoAlimentar.AlimentaEstaIdeia.Sas.Model
         /// <summary>
         /// Initializes a new instance of the <see cref="Tenant"/> class.
         /// </summary>
-        /// <param name="id">The id.</param>
-        /// <param name="name">The name.</param>
-        /// <param name="publicId">The public identifier.</param>
-        /// <param name="created">The created.</param>
-        /// <param name="invoicingStrategy">The invoicing strategy.</param>
-        /// <param name="paymentStrategy">The payment strategy.</param>
-        /// <param name="invoiceConfiguration">The invoice configuration.</param>
-        public Tenant(int id, string name, Guid publicId, DateTime created, InvoicingStrategy invoicingStrategy, PaymentStrategy paymentStrategy, InvoiceConfiguration invoiceConfiguration)
-        {
-            (this.Id, this.Name, this.PublicId, this.Created, this.InvoicingStrategy, this.PaymentStrategy, this.InvoiceConfiguration) =
-                (id, name, publicId, created, invoicingStrategy, paymentStrategy, invoiceConfiguration);
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Tenant"/> class.
-        /// </summary>
         public Tenant()
         {
             this.PaymentStrategy = PaymentStrategy.SharedPaymentProcessor;
