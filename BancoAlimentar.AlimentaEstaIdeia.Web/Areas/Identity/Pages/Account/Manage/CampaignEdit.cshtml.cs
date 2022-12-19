3f9fae874b7e821a529a9fb63cc070564bdd4f18
Luis Guerrero Guirado <guerrerotook@outlook.com>
2022-12-19 16:24:27 +0100

    Fix build issue on the branch.

--- BancoAlimentar.AlimentaEstaIdeia.Web/Areas/Identity/Pages/Account/Manage/CampaignEdit.cshtml.cs
+++ BancoAlimentar.AlimentaEstaIdeia.Web/Areas/Identity/Pages/Account/Manage/CampaignEdit.cshtml.cs
@@ -6,85 +6,83 @@
 
 namespace BancoAlimentar.AlimentaEstaIdeia.Web.Areas.Identity.Pages.Account.Manage
 {
-#pragma warning disable SA1027 // Use tabs correctly
-	using System.Threading.Tasks;
-	using BancoAlimentar.AlimentaEstaIdeia.Model;
-	using BancoAlimentar.AlimentaEstaIdeia.Model.Identity;
-	using Microsoft.AspNetCore.Identity;
-	using Microsoft.AspNetCore.Mvc;
-	using Microsoft.AspNetCore.Mvc.RazorPages;
-	using Microsoft.EntityFrameworkCore;
+    using System.Threading.Tasks;
+    using BancoAlimentar.AlimentaEstaIdeia.Model;
+    using BancoAlimentar.AlimentaEstaIdeia.Model.Identity;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.EntityFrameworkCore;
 
-	/// <summary>
-	/// Edit Referral campaign details.
-	/// </summary>
-	public class CampaignEditModel : PageModel
+    /// <summary>
+    /// Edit Referral campaign details.
+    /// </summary>
+    public class CampaignEditModel : PageModel
     {
-		private readonly ApplicationDbContext context;
-		private readonly UserManager<WebUser> userManager;
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<WebUser> userManager;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="CampaignEditModel"/> class.
-		/// </summary>
-		/// <param name="context">Unit of work.</param>
-		/// <param name="userManager">User Manager.</param>
-		public CampaignEditModel(ApplicationDbContext context, UserManager<WebUser> userManager)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CampaignEditModel"/> class.
+        /// </summary>
+        /// <param name="context">Unit of work.</param>
+        /// <param name="userManager">User Manager.</param>
+        public CampaignEditModel(ApplicationDbContext context, UserManager<WebUser> userManager)
         {
-			this.context = context;
-			this.userManager = userManager;
-		}
+            this.context = context;
+            this.userManager = userManager;
+        }
 
-		/// <summary>
-		/// Gets or sets the current referral.
-		/// </summary>
-		[BindProperty]
-		public Referral Referral { get; set; }
+        /// <summary>
+        /// Gets or sets the current referral.
+        /// </summary>
+        [BindProperty]
+        public Referral Referral { get; set; }
 
-		/// <summary>
-		/// Executes the get operation.
-		/// </summary>
-		/// <param name="id">Referral id.</param>
-		/// <returns>Task.</returns>
-		public async Task<IActionResult> OnGetAsync(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+        /// <summary>
+        /// Executes the get operation.
+        /// </summary>
+        /// <param name="id">Referral id.</param>
+        /// <returns>Task.</returns>
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-			this.Referral = await context.Referrals.FirstOrDefaultAsync(m => m.Id == id);
+            this.Referral = await context.Referrals.FirstOrDefaultAsync(m => m.Id == id);
 
-			if (this.Referral == null)
-			{
-				return NotFound();
-			}
+            if (this.Referral == null)
+            {
+                return NotFound();
+            }
 
-			var user = await userManager.GetUserAsync(User);
-			if (!(user != null && userManager != null && user.Id == Referral.User?.Id))
-			{
-				return NotFound();
-			}
+            var user = await userManager.GetUserAsync(User);
+            if (!(user != null && userManager != null && user.Id == Referral.User?.Id))
+            {
+                return NotFound();
+            }
 
-			return Page();
-		}
+            return Page();
+        }
 
-		/// <summary>
-		/// Execute the post operation.
-		/// </summary>
-		/// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
-		public async Task<IActionResult> OnPostAsync()
-		{
-			if (!ModelState.IsValid)
-			{
-				return Page();
-			}
+        /// <summary>
+        /// Execute the post operation.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
-			context.Attach(Referral).State = EntityState.Modified;
+            context.Attach(Referral).State = EntityState.Modified;
 
-			await context.SaveChangesAsync();
+            await context.SaveChangesAsync();
 
-			return RedirectToPage("./CampaignsHistory");
-		}
-	}
+            return RedirectToPage("./CampaignsHistory");
+        }
+    }
 }
-#pragma warning restore SA1027 // Use tabs correctly
