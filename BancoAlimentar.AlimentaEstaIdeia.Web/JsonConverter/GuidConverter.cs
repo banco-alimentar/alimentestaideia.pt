c3863283b41e68a60a4e8384ac342c774db24c9e
Luis Guerrero <guerrerotook@users.noreply.github.com>
2023-09-28 16:24:10 +0200

    Added a new GuidConverter class to handle empty Guid in the json. Solve #748 (#749)

---
+++ BancoAlimentar.AlimentaEstaIdeia.Web/JsonConverter/GuidConverter.cs
@@ -0,0 +1,67 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GuidConverter.cs" company="Federação Portuguesa dos Bancos Alimentares Contra a Fome">
+// Copyright (c) Federação Portuguesa dos Bancos Alimentares Contra a Fome. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace BancoAlimentar.AlimentaEstaIdeia.Web.JsonConverter
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Default Guid converter.
+    /// </summary>
+    public class GuidConverter : Newtonsoft.Json.JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(Guid) == objectType;
+        }
+
+        /// <summary>
+        /// Read the guid from the json value.
+        /// </summary>
+        /// <param name="reader">A reference to the reader.</param>
+        /// <param name="objectType">Object type.</param>
+        /// <param name="existingValue">Existing value.</param>
+        /// <param name="serializer">Serializer.</param>
+        /// <returns>The Guid.</returns>
+        /// <exception cref="ArgumentException">Possible excepion.</exception>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonToken.Null:
+                    return Guid.Empty;
+                case JsonToken.String:
+                    string str = reader.Value as string;
+                    if (string.IsNullOrEmpty(str))
+                    {
+                        return Guid.Empty;
+                    }
+                    else
+                    {
+                        return new Guid(str);
+                    }
+
+                default:
+                    throw new ArgumentException("Invalid token type");
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (Guid.Empty.Equals(value))
+            {
+                writer.WriteValue(string.Empty);
+            }
+            else
+            {
+                writer.WriteValue((Guid)value);
+            }
+        }
+    }
+}
