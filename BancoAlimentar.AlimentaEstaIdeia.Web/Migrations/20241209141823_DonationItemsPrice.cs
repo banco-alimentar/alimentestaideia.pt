15de4de4a730772c5c9669a1200a09be9fea4a06
Luis Guerrero Guirado <guerrerotook@outlook.com>
2024-12-09 15:30:11 +0100

    Chanange the datatype for the DonationItems.

---
+++ BancoAlimentar.AlimentaEstaIdeia.Web/Migrations/20241209141823_DonationItemsPrice.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="20241209141823_DonationItemsPrice.cs" company="Federação Portuguesa dos Bancos Alimentares Contra a Fome">
+// Copyright (c) Federação Portuguesa dos Bancos Alimentares Contra a Fome. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+#nullable disable
+
+namespace BancoAlimentar.AlimentaEstaIdeia.Web.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class DonationItemsPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "DonationItems",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "DonationItems",
+                type: "decimal(5,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
