458b9ccd945c93288ea964cfb52ed5494aa64bf8
tiagonmas <tiagonmas@hotmail.com>
2026-06-21 13:17:34 +0100

    Fixing ReferralImageTests.cs uses .Where() on IQueryable<ReferralLinkOpen> but was missing using System.Linq;:

--- BancoAlimentar.AlimentaEstaldeia.Web.IntegrationTests/IntegrationTests/ReferralImageTests.cs
+++ BancoAlimentar.AlimentaEstaldeia.Web.IntegrationTests/IntegrationTests/ReferralImageTests.cs
@@ -8,6 +8,7 @@ namespace BancoAlimentar.AlimentaEstaldeia.Web.IntegrationTests.IntegrationTests
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Threading.Tasks;
